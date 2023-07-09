1dfe0fca1c18547c1504b8355eeaff939f3aa364
Mpho Matli <mphojames9@gmail.com>
2023-07-09 12:21:27 +0200

    update

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
-let display = document.getElementById('display');
+const display_1 = document.querySelector(".display_1");
+const display_2 = document.querySelector(".display_2");
+const results_display = document.querySelector(".display_3");
+const numberBtn = document.querySelectorAll(".number");
+const clearBtn = document.querySelector(".clearAll");
+const deleteBtn = document.querySelector(".deleteBtn");
+const operationBtn = document.querySelectorAll(".operationalBtn");
+const equalBtn = document.querySelector(".equlBtn");
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+let disNum1 = "";
+let disNum2 = "";
+let result = null;
+let haveDot = false;
+let lastOperation = "";
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'Clear': //This is for clearing the display.
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case 'Delete': //Deleting the innerText one at a time.
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
-});
+numberBtn.forEach(button => button.addEventListener("click", (e)=>{
+    if(e.target.innerText === "." && !haveDot){
+        haveDot = true;
+    }
+    else if(e.target.innerText === "." && haveDot){
+        return
+    }
+    disNum2 += e.target.innerText;
+    display_2.innerText = disNum2;
+}));
+
+operationBtn.forEach(operation => operation.addEventListener("click", (e)=>{
+    if(disNum2 === ""){
+        return;
+    }
+    haveDot = false;
+    const operation = e.target.innerText;
+    if(disNum2 && disNum1 && lastOperation){
+        mathOperation();
+    }
+    else{
+        result = parseFloat(disNum2);
+    }
+    lastOperation = operation;
+    clearVar(lastOperation);
+}))
+
+function clearVar(name){
+    disNum1 += disNum2 +" "+ name + "";
+    display_1.innerText = disNum1;
+    disNum2 = " ";
+    display_2.innerText =" ";
+    results_display.innerText = result;
+}
+
+function mathOperation(){
+    if(lastOperation === "x"){
+        result = parseFloat(result) * parseFloat(disNum2);
+    } else if(lastOperation === "+"){
+        result = parseFloat(result) + parseFloat(disNum2);
+    } else if(lastOperation === "-"){
+        result = parseFloat(result) - parseFloat(disNum2);
+    }
+    else if(lastOperation === "/"){
+        result = parseFloat(result) / parseFloat(disNum2);
+    }
+    else if(lastOperation === "%"){
+        result = parseFloat(result) % parseFloat(disNum2);
+    }
+    console.log(result)
+}
+    
+
+equalBtn.addEventListener("click",(e)=>{
+    mathOperation();
+    clearVar();
+})
